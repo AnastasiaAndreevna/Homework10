cc26dc5da0c37a1bd2cdcc668ff1712f8fee64e7
AnastasiaAndreevna <31144016+AnastasiaAndreevna@users.noreply.github.com>
2017-12-17 01:31:15 +0200

    initial commit

---
+++ source-js/main.js
@@ -0,0 +1,56 @@
+var number1
+var number2
+
+function add () {
+  return number1 + number2
+}
+
+console.log(add)
+
+function sub () {
+  return number1 - number2
+}
+
+console.log(sub)
+
+function multi () {
+  return number1 * number2
+}
+
+console.log(multi)
+
+function div () {
+  if (number2 = 0) {
+    number1 / number2
+  }
+}
+
+console.log(div)
+
+function exponentiation () {
+  return Math.pow(number1, number2)
+}
+
+console.log(exponentiation())
+
+function root () {
+  return Math.sqrt(number1)
+}
+
+console.log(root())
+
+function sin () {
+  return Math.sin(number1)
+}
+
+console.log(sin)
+
+function cos () {
+  return Math.cos(number1)
+}
+
+function factorial () {
+  return n ? n * factorial(n - 1) : 1
+}
+
+console.log(factorial(number1))
